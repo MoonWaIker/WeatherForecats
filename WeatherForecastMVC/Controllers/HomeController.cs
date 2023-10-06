63ca6199101b08429c3de4b65e10972b2ebb54fc
MoonWalker <muhinmihajlo40@gmail.com>
2023-10-06 14:09:52 +0300

    Added cookies

--- WeatherForecastMVC/Controllers/HomeController.cs
+++ WeatherForecastMVC/Controllers/HomeController.cs
@@ -24,9 +24,19 @@ namespace WeatherForecastMVC.Controllers
 
         public IActionResult WeatherForecast(string city)
         {
+            CookieOptions cookieOptions = new() { };
+            Response.Cookies.Append("City", city, cookieOptions);
+
             try
             {
                 WeatherForecastModel model = weatherForecast.GetWeatherForecast(city);
+
+                CookieOptions cookieOptions2 = new()
+                {
+                    Expires = DateTime.Now.AddDays(1)
+                };
+                Response.Cookies.Append("VistedCity", city, cookieOptions2);
+
                 return View(model);
             }
             catch (ArgumentNullException ex)
